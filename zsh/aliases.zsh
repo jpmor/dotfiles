2aea890464dcd28f6fb125288ee677a549adc0b0
James Moriarty <jmoriarty@rsglab.com>
2019-02-09 13:20:28 -0500

    Add gcm and sort.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,12 +1,14 @@
 #!/usr/bin/env zsh
 
 alias dot="cd $DOT"
-alias ls='exa'
+alias mc="cd $MC"
+alias pm="cd $MC/product/mailchimp"
+
+alias gcm='git commit -m'
 alias gd='git diff'
 alias gst='git status'
 alias hs='history'
-alias mc="cd $MC"
+alias ls='exa'
 alias path="echo $PATH | tr : '\n'"
-alias pm="cd $MC/product/mailchimp"
 alias pip3="pip3 --isolated"
 alias t2="tree -L 2"
