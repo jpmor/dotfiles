199e8bba95cc0d2fb04479ec59ccbc07879077aa
James Moriarty <jmoriarty@rsglab.com>
2019-02-09 19:37:46 -0500

    Typeset ZSH_HIGHLIGHT_STYLES for X11.

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -3,6 +3,7 @@
 local ret_status="%(?:%{$fg_bold[green]%}>:%{$fg_bold[red]%}>)"
 PROMPT='${ret_status} %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)'
 
+typeset -gA ZSH_HIGHLIGHT_STYLES
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}✗"
