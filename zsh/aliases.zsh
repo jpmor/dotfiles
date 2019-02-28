69b2c4ff7d8d40f06eb902579981cc3d27bb990c
James Moriarty <jmoriarty@gatech.edu>
2019-02-27 20:09:54 -0500

    Add some mc repo aliases and k for kubectl.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,12 +2,15 @@
 
 alias dot="cd $DOT"
 alias mc="cd $MC"
+alias mcgo="cd $MCGO"
 alias pm="cd $MC/product/mailchimp"
+alias op="cd $MC/ops/puppet-modules"
 alias hw="cd $HOME/homewiki"
 
 alias brewup='brew update && brew upgrade'
 alias grep='grep --color=auto'
 alias hs='history 1'
+alias k='kubectl'
 alias ls='exa'
 alias path="echo $PATH | tr : '\n'"
 alias pip3="pip3 --isolated"
