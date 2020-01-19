a1c93f417c62efce03f515d10ca2c1cf91301974
James Moriarty <jmoriarty@rsglab.com>
2020-01-18 22:30:02 -0500

    Clean up zsh aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+# directory jumps
 alias dot="cd $DOT"
 alias hw="cd $HOME/homewiki"
 alias sw="cd $HOME/side/site"
@@ -7,19 +8,23 @@ alias mc="cd $MC"
 alias mcgo="cd $MCGO"
 alias op="cd $MC/ops/puppet-modules"
 alias pm="cd $MC/product/mailchimp"
+alias rsg="cd $MC/rsg/"
 
-alias brewup='brew update && brew upgrade'
-alias grep='grep --color=auto'
-#alias hs='history 1 | grep'
-alias k='kubectl'
+# adjustments
 alias ls='exa'
-alias path="echo $PATH | tr : '\n'"
+alias jq="jq -r" # why is this not the default???
+alias grep='grep --color=auto'
 alias pip3="pip3 --isolated"
 alias sed="gsed"
-alias t2="tree -L 2"
 
+# useful
+alias brewup='brew update && brew upgrade'
+alias path="echo $PATH | tr : '\n'"
+alias t2="tree -L 2"
 alias tags='rg -e ":[a-z]*:" -o -N --no-filename $HOME/homewiki/log | sort | uniq -c'
 
+# shortcuts
+alias k='kubectl'
 alias gcm='git commit -m'
 alias gd='git diff'
 alias gst='git status'
