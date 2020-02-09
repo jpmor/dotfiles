20f741558db6238cbeac0cd5b315e69149c6a3e1
James Moriarty <jmoriarty@rsglab.com>
2020-02-09 16:00:14 -0500

    Add forking to setup; Create vipssh

--- zsh/funcs.zsh
+++ zsh/funcs.zsh
@@ -1,19 +1,23 @@
 #!/usr/bin/env zsh
 
 # args: org, repo
-# need to clone in GHE first
 setup() {
-  origin_ssh_url=git@git.rsglab.com:$USER/$2.git; \
-  upstream_ssh_url=git@git.rsglab.com:$1/$2.git; \
-  mkdir -p $MC/$1; \
-  git clone $origin_ssh_url $MC/$1/$2; \
-  git -C $MC/$1/$2 remote add upstream $upstream_ssh_url; \
+  ghe_api_url="https://git.rsglab.com/api/v3/repos/$1/$2/forks"
+  curl -o /dev/null -su "$USER:$(cat ~/.ghe)" -X POST $ghe_api_url # make a fork
+  sleep 10 # wait cuz forking is async
+
+  origin_url=git@git.rsglab.com:$USER/$2.git
+  mkdir -p $MC/$1
+  git clone $origin_url $MC/$1/$2; # clone fork as origin
+
+  upstream_url=git@git.rsglab.com:$1/$2.git
+  git -C $MC/$1/$2 remote add upstream $upstream_url # add upstream as upstream
   echo
-  echo "******** REMOTES: ********"; \
-  git -C $MC/$1/$2 remote -vv; \
-  echo "**************************"; \
+  echo "******** REMOTES: ********"
+  git -C $MC/$1/$2 remote -vv
+  echo "**************************"
   echo
-  echo "Setup Complete!!"; \
+  echo "Setup Complete!!"
 }
 
 gitsync() {
@@ -44,6 +48,36 @@ tag() {
   fi
 }
 
-peaches() {
+p() {
   open "https://us1.admin.mailchimp.com/peaches2/tools/user-search/direct-search?search-input=$1"
+}
+
+newbugs() {
+  curl -s -H "X-Bugsnag-API: true" -H "X-Version: 2" -H "Authorization: token $(cat ~/.bstoken)" "https://api.bugsnag.com/projects/5d40577f2103c00011aa3a7f/errors?&sort=first_seen&direction=desc" | jq '.[0:5][]|{first_seen: .first_seen, context: .context, message: .message, users: .users, id: .id}'
+}
+
+vipssh() {
+  vip="$1"
+  host=$(dcm vips list | grep $vip | awk '{ print $1; }' | sed 's/\([a-z0-9]*\)-\([a-z0-9]*\)-.*/\1.\2.rsglab.com/g')
+
+  for i in {1..2}; do
+    sfthost="db$i-$host"
+    echo $sfthost
+    vips=$(ssh $sfthost 'hostname -I')
+    echo $vips
+    if test "${vips#*$vip}" != "$vips"
+    then
+      current_host=$sfthost
+      break
+    fi
+  done
+
+  ssh $current_host
+}
+
+mcurl() {
+  seq 1 20 | parallel \
+    'shardkey="$(jq -r .us{} $HOME/.howler.json)"
+    shardcookie="$HOME/.cookies/us{}"
+    curl -sk -X POST "https://us{}.admin.mailchimp.com/login/post/" -d  $shardkey -c $shardcookie'
 }
