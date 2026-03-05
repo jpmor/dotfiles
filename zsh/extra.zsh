074c823a6a326e81771049f0c4d46c3eb456f886
James Moriarty <james@moriartys.co>
2026-03-04 22:13:44 -0500

    Gate personal/intuit across funcs, extra, vimrc, gitconfig; rename mc.vim -> intuit.vim
    - funcs.zsh: tag/mdtag personal, kea/ken/ke1 intuit, rest shared
    - extra.zsh: gcloud sourcing gated to intuit
    - vimrc: conditionally source intuit.vim / wiki.vim by DOTPROFILE
    - gitconfig: move postBuffer to gitconfig.intuit
    - Rename mc.vim -> intuit.vim

--- zsh/extra.zsh
+++ zsh/extra.zsh
@@ -6,7 +6,10 @@ source <(fzf --zsh)
 if [[ "$OSTYPE" == "darwin"* ]]; then
   source "$HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
   source "$HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-  source "$HOMEBREW_PREFIX/share/google-cloud-sdk/path.zsh.inc"
-  source "$HOMEBREW_PREFIX/share/google-cloud-sdk/completion.zsh.inc"
   FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+
+  if [[ $DOTPROFILE == intuit ]]; then
+    source "$HOMEBREW_PREFIX/share/google-cloud-sdk/path.zsh.inc"
+    source "$HOMEBREW_PREFIX/share/google-cloud-sdk/completion.zsh.inc"
+  fi
 fi
