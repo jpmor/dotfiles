c924d732d591206662937a6758168523135041fc
James Moriarty <james@moriartys.co>
2026-03-16 00:28:02 -0400

    aliases: add budget alias for homewiki tools/budget/budget

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -43,6 +43,7 @@ alias t2='tree -L 2'
 if [[ $DOTPROFILE == personal ]]; then
   alias wt="$HW/tools/wt"
   alias flow="$HW/tools/flow"
+  alias budget="$HW/tools/budget/budget"
   alias today='vim $(find $HW/log/*$(date +-%m-%d).md)'
 fi
 
